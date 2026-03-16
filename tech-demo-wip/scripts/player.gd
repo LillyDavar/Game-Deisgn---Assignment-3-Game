7ab276862a3d26f0bc9c3bd3632ba70640974e39
LillyDavar <lilly.scott@uleth.ca>
2026-03-16 11:54:28 -0600

    Version 0.4
    Added Main menu, fixed some bugs involving the first conversation.

---
+++ tech-demo-wip/scripts/player.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+
+var last_direction: Vector2 = Vector2.DOWN
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+	process_movement()
+	process_animation(last_direction)
+	move_and_slide()
+	
+func process_movement() -> void:
+# setup direction of movement
+	var direction = Input.get_vector("left", "right", "up", "down")
+# stop diagonal movement by listening for input then setting axis to zero
+	if Input.is_action_pressed("right") || Input.is_action_pressed("left"):
+		direction.y = 0
+	elif Input.is_action_pressed("up") || Input.is_action_pressed("down"):
+		direction.x = 0
+	else:
+		direction = Vector2.ZERO
+
+	
+#normalize the directional movement
+	direction = direction.normalized()
+# setup the actual movement
+	if direction != Vector2.ZERO:
+		velocity = (direction * SPEED)
+		last_direction = direction
+	else:
+		velocity = Vector2.ZERO
+	
+
+func process_animation(direction) -> void:
+	if velocity != Vector2.ZERO:
+		play_animation("walking", direction)
+	else:
+		play_animation("idle", direction)
+
+func play_animation(prefix: String, dir: Vector2) -> void:
+	if dir.x > 0:
+		animated_sprite_2d.play(prefix + "_right")
+	elif dir.x < 0:
+		animated_sprite_2d.play(prefix + "_left")
+	elif dir.y < 0:
+		animated_sprite_2d.play(prefix + "_up")
+	elif dir.y > 0:
+		animated_sprite_2d.play(prefix + "_down")
