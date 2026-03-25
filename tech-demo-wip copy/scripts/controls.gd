e588c8d1b0c8cb3c422551aed1e8be10290c828b
Lilly Scott <lilly.scott@uleth.ca>
2026-03-25 09:43:58 -0600

    Version 1.0.4
    Added more temp music, added game control screen, implemented final sprites for select items, patched bug where you can walk into void, made Reds dialogue play automatically for the sake of the demo.

--- tech-demo-wip copy/scripts/controls.gd
+++ tech-demo-wip copy/scripts/controls.gd
@@ -3,10 +3,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Dialogic.start("res://Timelines/Conversations/Player Name.dtl")
-
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_texture_button_pressed() -> void:
+	Dialogic.start("res://Timelines/Conversations/Player Name.dtl")
--- tech-demo-wip copy/scripts/main_menu.gd
+++ tech-demo-wip copy/scripts/main_menu.gd
@@ -46,7 +46,7 @@ func pressed():
 	var button = getButton()
 	
 	if button.name == "New Game":
-		get_tree().change_scene_to_file("res://Scenes/Name.tscn") 
+		get_tree().change_scene_to_file("res://Scenes/Controled_scenes/Controls.tscn") 
 	elif button.name == "Continue":
 		get_tree().change_scene_to_file("res://Scenes/Maps/game_screen.tscn")
 	elif button.Name == "Quit":
