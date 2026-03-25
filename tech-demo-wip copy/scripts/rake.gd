f2c0b9ca28ed0ddcff7537694aff246a8b336cb9
LillyDavar <lilly.scott@uleth.ca>
2026-03-25 01:07:31 -0600

    Version 1.0.2
    fixed bug where the rake shows the fridge interaction

---
+++ tech-demo-wip copy/scripts/rake.gd
@@ -0,0 +1,10 @@
+extends StaticBody2D
+
+@onready var interactable: Area2D = $interactable
+@onready var sprite_2d: Sprite2D
+
+func _ready() -> void:
+	interactable.interact = _on_interact
+	
+func _on_interact():
+	Dialogic.start("res://Timelines/Interactions/Rake.dtl")
