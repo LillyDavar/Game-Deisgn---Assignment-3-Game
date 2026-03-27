0171a87e0254b11447dd0c45d83d089f933864e3
Lilly Scott <lilly.scott@uleth.ca>
2026-03-26 20:03:10 -0600

    Version 1.1.5
    Fixed several file names, added hat to scenes, Refined the controls screen's text. Added some text to Hat dialogues

--- tech-demo-wip copy/Scenes/Controled_scenes/v_box_container.gd
+++ tech-demo-wip copy/Scenes/Controled_scenes/v_box_container.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends VBoxContainer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,10 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-
-func _on_texture_button_pressed() -> void:
-	
-	Dialogic.start("res://Timelines/Conversations/Player Name.dtl")
-	
+	grab_focus()
